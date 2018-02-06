1263c4a2d30b8852ebf8027ec8a2684e0a461ddb
GRADY KNIGHT <gradyknight@gradys-mbp.lan>
2018-02-05 23:58:37 -0500

    framework js logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,131 @@
+// GAME INITIALIZATION
+// initialize each character with characteristics and initialize general variables
+//GLOBAL VARIABLES
+var hasUserChosenCharacter;
+var charOne;
+var charTwo;
+var charThree;
+var charFour;
+var usersChoice;
+var allCharacters;
+var enemiesArray;
+
+
+
+function ResetGame(){
+    var hasUserChosenCharacter = false;
+
+    var charOne = {
+        name: "cow",
+        HealthPower: 100,
+        AttackPower:10,
+        CounterAttackPower=10,
+        IncreaseAttackPower: function(){
+            AttackPower=AttackPower+CounterAttackPower;
+        },
+        imageName:"one.png",
+        index:0;
+    };
+    
+    var charTwo = {
+        name: "donkey",
+        HealthPower: 80,
+        AttackPower:15,
+        CounterAttackPower=7,
+        IncreaseAttackPower: function(){
+            AttackPower=AttackPower+CounterAttackPower;
+        },
+        imageName: "two.png",
+        index:1
+    };
+    
+    var charThree = {
+        name: "chicken",
+        HealthPower: 40,
+        AttackPower:5,
+        CounterAttackPower=30,
+        IncreaseAttackPower: function(){
+            AttackPower=AttackPower+CounterAttackPower;
+        },
+        imageName:"three.png",
+        index:2
+    };
+    
+    var charFour = {
+        name: "pig",
+        HealthPower: 250,
+        AttackPower:9,
+        CounterAttackPower=19,
+        IncreaseAttackPower: function(){
+            AttackPower=AttackPower+CounterAttackPower;
+        },
+        imageName:"four.png",
+        index:3
+    };
+    var allCharacters=[charOne, charTwo, charThree, charFour];
+    var usersChoice="";
+    var enemiesArray=[];
+}
+
+// assign character to usersChoice on click of icon. index value of user's chosen character is passed
+function AssignCharactersToGroups(indexValue){
+    usersChoice = allCharacters[indexValue];
+    allCharacters.slice(indexValue,1);
+    for (var i=0; i<allCharacters.length;i++){
+        enemiesArray[i]=allCharacters[i];
+    }
+    // Move user character to user div
+    // user character --> user div
+
+    // move other characters to enemies div
+    // for each in enemies array --> enemies div
+    // update GUI
+}
+
+
+function PlayRoundWhereEnemyIsClicked(enemy){
+    usersChoice.HealthPower -= enemy.AttackPower;
+    enemy.HealthPower -= usersChoice.AttackPower;
+    usersChoice.AttackPower += usersChoice.CounterAttackPower;
+}
+
+function IsCharacterDead(theCharacter){
+    if (theCharacter.HealthPower<=0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function EvaluateResultOfRound(){
+    if (IsCharacterDead(usersChoice)) {
+        console.log("game over. user lost");
+    } else if (IsCharacterDead(enemiesArray[indexChosen])) {
+        console.log("enemy died");
+    }
+}
+
+// update GUI
+function RemoveCharacterFromDiv(theCharacter, theDiv){
+
+}
+
+// GAME TERMINIATION
+// change winning text
+function UpdateStatusTextWithMessage(theString) {
+    
+};
+// draw button to restart
+function DrawRestartButton(){
+
+}
+
+function PlayTheGame() {
+    ResetGame();
+    if (!hasUserChosenCharacter) {
+        // on click
+        AssignCharactersToGroups(0); // !! need to get index value of character
+    }
+
+    }
+}
