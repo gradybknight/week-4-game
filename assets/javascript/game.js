dffac81373095dc3ac0d4ac4070e0fa64bc82f06
GRADY KNIGHT <gradyknight@gradys-mbp.lan>
2018-02-11 15:36:20 -0500

    moving tiles for chosen characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,67 +19,122 @@ function ResetGame(){
         name: "cow",
         HealthPower: 100,
         AttackPower:10,
-        CounterAttackPower=10,
+        CounterAttackPower:10,
         IncreaseAttackPower: function(){
             AttackPower=AttackPower+CounterAttackPower;
         },
         imageName:"cow.jpg",
-        index:0
+        index:0,
+        currentLocation:"notChosen"
     };
     
     var charTwo = {
         name: "donkey",
         HealthPower: 80,
         AttackPower:15,
-        CounterAttackPower=7,
+        CounterAttackPower:7,
         IncreaseAttackPower: function(){
             AttackPower=AttackPower+CounterAttackPower;
         },
         imageName: "donkey.jpg",
-        index:1
+        index:1,
+        currentLocation:"notChosen"
     };
     
     var charThree = {
         name: "chicken",
         HealthPower: 40,
         AttackPower:5,
-        CounterAttackPower=30,
+        CounterAttackPower:30,
         IncreaseAttackPower: function(){
             AttackPower=AttackPower+CounterAttackPower;
         },
         imageName:"chicken.jpg",
-        index:2
+        index:2,
+        currentLocation:"notChosen"
     };
     
     var charFour = {
         name: "pig",
         HealthPower: 250,
         AttackPower:9,
-        CounterAttackPower=19,
+        CounterAttackPower:19,
         IncreaseAttackPower: function(){
             AttackPower=AttackPower+CounterAttackPower;
         },
         imageName:"pig.jpg",
-        index:3
+        index:3,
+        currentLocation:"notChosen"
     };
-    var allCharacters=[charOne, charTwo, charThree, charFour];
-    var usersChoice="";
-    var enemiesArray=[];
+    allCharacters=[charOne, charTwo, charThree, charFour];
+    usersChoice="";
+    enemiesArray=[];
+    AssignAllCharactersToStartingDiv();
+
+    $('#cow').click(MoveCow);
+    function MoveCow(){
+        console.log($('#cow').attr("currentLocation"));
+        var chosenCharacterIndex = $('#cow').attr("index");
+        AssignCharactersToGroups(chosenCharacterIndex);
+    }
+    $('#chicken').click(MoveChicken);
+    function MoveChicken(){
+        console.log($('#chicken').attr("currentLocation"));
+        var chosenCharacterIndex = $('#chicken').attr("index");
+        AssignCharactersToGroups(chosenCharacterIndex);
+    }
+    $('#donkey').click(MoveDonkey);
+    function MoveDonkey(){
+        console.log($('#donkey').attr("currentLocation"));
+        var chosenCharacterIndex = $('#donkey').attr("index");
+        AssignCharactersToGroups(chosenCharacterIndex);
+    }
+    $('#pig').click(MovePig);
+    function MovePig(){
+        console.log($('#pig').attr("currentLocation"));
+        var chosenCharacterIndex = $('#pig').attr("index");
+        AssignCharactersToGroups(chosenCharacterIndex);
+    }
+}
+
+function AssignAllCharactersToStartingDiv(){
+    for (var i= 0; i<allCharacters.length; i++){
+        var characterTile = $("<div>");
+        characterTile.attr("id", allCharacters[i].name);
+        characterTile.attr("index", i);
+        characterTile.attr("currentLocation", "notChosen");
+        characterTile.html('<img src = ./assets/images/' + allCharacters[i].imageName + ' width = 150px">');
+        $("#notChosenCharacterDiv").append(characterTile);
+    }
 }
 
 // assign character to usersChoice on click of icon. index value of user's chosen character is passed
 function AssignCharactersToGroups(indexValue){
     usersChoice = allCharacters[indexValue];
-    allCharacters.slice(indexValue,1);
     for (var i=0; i<allCharacters.length;i++){
-        enemiesArray[i]=allCharacters[i];
+        if (allCharacters[i]!== usersChoice){
+            enemiesArray.push(allCharacters[i]);
+        }
     }
     // Move user character to user div
     // user character --> user div
-
+    var characterTile = $("<div>");
+    characterTile.attr("id", usersChoice.name);
+    characterTile.attr("index", usersChoice.index);
+    characterTile.attr("currentLocation", "usersCharacterDiv");
+    characterTile.html('<img src = ./assets/images/' + usersChoice.imageName + ' width = 150px">');
+    $("#usersCharacterDiv").append(characterTile);
     // move other characters to enemies div
-    // for each in enemies array --> enemies div
-    // update GUI
+    for (var i = 0; i<enemiesArray.length;i++){
+        var characterTile = $("<div>");
+        characterTile.attr("id", enemiesArray[i].name);
+        characterTile.attr("index", enemiesArray[i].index);
+        characterTile.attr("currentLocation", "enemyCharacterDiv");
+        characterTile.html('<img src = ./assets/images/' + enemiesArray[i].imageName + ' width = 150px">');
+        $("#enemyCharacterDiv").append(characterTile);
+    }
+    // clear not chosen div
+    $('#notChosenCharacterDiv').empty();
 }
 
 
@@ -127,3 +182,8 @@ function PlayTheGame() {
         AssignCharactersToGroups(0); // !! need to get index value of character
     }
 };
+
+
+
+
+ResetGame();
